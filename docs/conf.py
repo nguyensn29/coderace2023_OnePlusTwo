e8c7466aa47798b852058a5f2bdb2192bc487d8b
hoangteo0103 <51512556+hoangteo0103@users.noreply.github.com>
2023-06-15 21:13:55 +0700

    parent 83d1eef437b0be20ee4b1ca48d0f4c07041aacc3 author hoangteo0103 <51512556+hoangteo0103@users.noreply.github.com> 1686838435 +0700 committer Bui Nguyen Hoang <51512556+hoangteo0103@users.noreply.github.com> 1686887864 +0700
    File added

--- docs/conf.py
+++ docs/conf.py
@@ -32,9 +32,8 @@ html_static_path = ['_static']
 needs_types = [
                dict(directive="sys_req", title="System Requirement", prefix="SYSTEM_REQ_", color="#ac6dd1", style="artifact"),
                dict(directive="sw_req", title="Software Requirement", prefix="SOFTWARE_REQ_", color="#ac6dd1", style="artifact"),
-               dict(directive="verify", title="Verification Criteria", prefix="VC_", color="#fedcd2", style="artifact")             
+               dict(directive="verify", title="Verification Criteria", prefix="VC_", color="#fedcd2", style="artifact")
                ]
 
 needs_extra_options = ['created_by', 'url',  'date', 'time',  'safety_level',  'artifact_type', 'crq',
                         'test_level']
-
--- docs/index.rst
+++ docs/index.rst
@@ -1,16 +1,16 @@
-.. Lesson01 documentation master file, created by
-   sphinx-quickstart on Thu Mar  2 21:03:24 2023.
+.. Test_CodeRace documentation master file, created by
+   sphinx-quickstart on Wed Jun 14 15:50:23 2023.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Hello World 1
-====================================
+Welcome to Test_CodeRace's documentation!
+=========================================
 
 .. toctree::
-   :maxdepth: 2
-   :caption: System Requirements
+   :maxdepth: 1
+   :caption: Contents:
 
-   ./src/testing.rst
+   ./src/testing
 
 Indices and tables
 ==================
